e7383355453c21889d104b1b5b9f176ed4243c5a
sievd <sievd123@gmail.com>
2021-03-23 05:40:04 +0100

    Update DB scripts

--- back/data/seed.sql
+++ back/data/seed.sql
@@ -5,13 +5,32 @@ INSERT INTO users VALUES
     ("user-1", "user-1@example.com", "User 1", '{hash_password["user-1-password"]}', 0),
     ("user-2", "user-2@example.com", "User 2", '{hash_password["user-2-password"]}', 0),
     ("user-3", "user-3@example.com", "User 3", '{hash_password["user-3-password"]}', 0),
-    ("user-4", "user-4@example.com", "User 4", '{hash_password["user-4-password"]}', 1);
+    ("admin-1", "admin-1@example.com", "Admin-1", '{hash_password["admin-1-password"]}', 1);
 
 DELETE FROM goals;
 INSERT INTO goals VALUES 
-    ("test-id-1", "test-title", "test-category", "test-status", "user-1"),
-    ("test-id-2", "test-title", "test-category", "test-status", "user-1"),
-    ("test-id-3", "test-title", "test-category", "test-status", "user-1"),
-    ("test-id-4", "test-title", "test-category", "test-status", "user-2");
+    ("test-id-1", "test-date", "test-title", "test-category-1", "test-status", "user-1"),
+    ("test-id-2", "test-date", "test-title", "test-category-1", "test-status", "user-1"),
+    ("test-id-3", "test-date", "test-title", "test-category-2", "test-status", "user-1"),
+    ("test-id-4", "test-date", "test-title", "test-category-2", "test-status", "user-2");
+
+DELETE FROM tasks;
+INSERT INTO tasks VALUES
+    ("test-id-1", "test-title", "test-description", "test-hint", "test-goal-id-1"),
+    ("test-id-2", "test-title", "test-description", "test-hint", "test-goal-id-1");
+
+DELETE FROM logs;
+INSERT INTO logs VALUES
+    ("2021-03-04T08:47:19Z", "user-1"),
+    ("2021-03-04T08:50:23Z", "user-1"),
+    ("2021-03-05T08:50:23Z", "user-1"),
+    ("2021-03-06T08:50:23Z", "user-1"),
+    ("2021-03-06T08:50:23Z", "user-2");
+
+DELETE FROM admin_users;
+INSERT INTO admin_users VALUES
+    ("admin-1", "user-1"),
+    ("admin-1", "user-2"),
+    ("admin-1", "user-3");
 
 COMMIT;
--- back/data/structure.sql
+++ back/data/structure.sql
@@ -12,11 +12,38 @@ CREATE TABLE users (
 DROP TABLE IF EXISTS goals;
 CREATE TABLE IF NOT EXISTS goals (
     id varchar,
+    date varchar,
     title varchar,
     category varchar,
     status varchar,
     user_id varchar,
     PRIMARY KEY(id),
+    FOREIGN KEY ("user_id") REFERENCES users("user_id")
+);
+
+DROP TABLE IF EXISTS tasks;
+CREATE TABLE IF NOT EXISTS tasks (
+    id varchar,
+    title varchar,
+    description varchar,
+    hint varchar,
+    goal_id varchar,
+    PRIMARY KEY(id),
+    FOREIGN KEY ("goal_id") REFERENCES goals("goal_id")
+);
+
+DROP TABLE IF EXISTS logs;
+CREATE TABLE IF NOT EXISTS logs (
+    timestamp varchar,
+    user_id varchar,
+    FOREIGN KEY ("user_id") REFERENCES users("user_id")
+);
+
+DROP TABLE IF EXISTS admin_users;
+CREATE TABLE IF NOT EXISTS admin_users (
+    admin_id varchar,
+    user_id varchar,
+    FOREIGN KEY ("admin_id") REFERENCES users("user_id"),
     FOREIGN KEY ("user_id") REFERENCES users("user_id")
 );
 
