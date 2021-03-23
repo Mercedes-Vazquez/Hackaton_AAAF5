2f3b3471ae3dd8a97e498d12ce6bb912fb9e226c
Accountant77 <vbonilla.77@hotmail.com>
2021-03-23 09:08:35 +0100

    primer commit

--- front/src/views/Home/HomePage.vue
+++ front/src/views/Home/HomePage.vue
@@ -2,15 +2,23 @@
   <div class="home-page">
     <h1>This is the home page</h1>
     <HelloWorld :msg="message" />
+    <Frecuency />
+    <TaskListPage />
+
   </div>
 </template>
 
 <script>
 import HelloWorld from "./HelloWorld.vue";
-
+import TaskListPage from "../TaskList/TaskListPage.vue";
+import Frecuency from "../TaskList/Frecuency.vue";
 export default {
   name: "Home",
-  components: { HelloWorld },
+  components:{ 
+    HelloWorld,
+    TaskListPage,
+    Frecuency,
+    },
   data() {
     return {
       message: "Hello, world!",
